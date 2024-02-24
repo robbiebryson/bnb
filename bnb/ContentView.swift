0980b9e225cef3bdfc98ee4882cdb2b9b20565e2
robbiebryson <65382229+robbiebryson@users.noreply.github.com>
2024-02-24 08:42:52 -0500

    Initial Commit

---
+++ bnb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bnb
+//
+//  Created by Rob Bryson on 2/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bnb/bnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  bnbApp.swift
+//  bnb
+//
+//  Created by Rob Bryson on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct bnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
