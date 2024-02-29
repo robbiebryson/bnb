2e3df436b35eaefae04fac6fd357cd35575a6711
robbiebryson <65382229+robbiebryson@users.noreply.github.com>
2024-02-28 22:47:19 -0500

    Updated DestinationSearchView
    Added DatePicker and Stepper - solved bug relating to onTapGestures in iOS 17.1

--- bnb/Core/Explore/DestinationSearchView.swift
+++ bnb/Core/Explore/DestinationSearchView.swift
@@ -18,6 +18,9 @@ struct DestinationSearchView: View {
     @Binding var show: Bool
     @State private var destination = ""
     @State private var selectedOption: destinationSearchOptions = .location
+    @State private var startDate = Date()
+    @State private var endDate = Date()
+    @State private var numGuests = 0
     
     var body: some View {
         VStack {
@@ -76,20 +79,36 @@ struct DestinationSearchView: View {
             
 //            date selection view
             
-            VStack {
+            VStack(alignment: .leading) {
                 if selectedOption == .dates {
-                    HStack {
-                        Text("Show Expanded View")
+                        Text("When's your trip?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    
+                    VStack {
+                        DatePicker("From", selection: $startDate, displayedComponents: .date)
+                            .onTapGesture(count: 99, perform: {
+                                       // overrides tap gesture to fix ios 17.1 bug
+                                   })
                         
-                        Spacer()
-
+                        Divider()
+                        
+                        DatePicker("To", selection: $endDate, displayedComponents: .date)
+                            .onTapGesture(count: 99, perform: {
+                                       // overrides tap gesture to fix ios 17.1 bug
+                                   })
+                        
                     }
+                    .foregroundStyle(.secondary)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
                 } else {
                     CollapsedPickerView(title: "When", description: "Add Dates")
                 }
             }
             .padding()
-            .frame(height: selectedOption == .dates ? 120 : 64)
+            .frame(height: selectedOption == .dates ? 180 : 64)
             .background(.background)
             .clipShape(RoundedRectangle(cornerRadius: 12))
             .padding()
@@ -100,13 +119,23 @@ struct DestinationSearchView: View {
             
             
 //            number of guests view
-            VStack {
+            VStack(alignment: .leading) {
                 if selectedOption == .guests {
-                    HStack {
-                        Text("Show Expanded View")
-                        
-                        Spacer()
-                    }
+                    Text("Who's Coming?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    
+                    Stepper {
+                        Text(" \(numGuests) Adults")
+                    } onIncrement: {
+                        numGuests += 1
+                    } onDecrement: {
+                        guard numGuests > 0 else {return}
+                        numGuests -= 1
+                    }.onTapGesture(count: 99, perform: {
+                        // overrides tap gesture to fix ios 17.1 bug
+                    })
                 } else {
                     CollapsedPickerView(title: "Who", description: "Add Guests")
                 }
